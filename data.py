f5652b832c8de19116d603b17047af5c234a57fe
oscaralfonso17 <oscaralfonsofloreso@hotmail.com>
2020-08-31 07:13:58 -0500

    Updates

--- data.py
+++ data.py
@@ -8,74 +8,118 @@
 # -- repository: https://github.com/oscaralfonso17/myst_if710183_lab1.git                                -- #
 # -- --------------------------------------------------------------------------------------------------- -- #
 """
+
+# --------------------------------------- CLASE --------------------------------------------------
+from os import listdir, path
+from os.path import isfile, join
 import pandas as pd
-import datetime
-from functions import last_day_of_month, datareader
 import numpy as np
+import time
+import yfinance as yf
+from datetime import datetime
+import math
 
-# timestamp part of df_pasiva
-dates = []
-[dates.append(last_day_of_month(datetime.date(2018, month, 1))) for month in range(1, 13)]
-[dates.append(last_day_of_month(datetime.date(2019, month, 1))) for month in range(1, 13)]
-[dates.append(last_day_of_month(datetime.date(2020, month, 1))) for month in range(1, 9)]
-dates = pd.DataFrame(dates)
-dates.columns = ['timestamp']
+abspath = path.abspath('files/NAFTRAC_holdings')
 
-# Data reader
-path = 'files/NAFTRAC_holdings'
-all_files = datareader(path)
+# Crear diccionario para almacenar todos los datos
+data_archivos = {}
+archivos = [f[:-4] for f in listdir(abspath) if isfile(join(abspath, f))]
+# Ordernar archivos por fecha
+archivos = sorted(archivos, key=lambda t: datetime.strptime(t[8:], '%d%m%y'))
 
-# Initial %: first all_files file readed
-pesos_in = pd.read_csv(all_files[0], skiprows=2)
-pesos_in = pesos_in.loc[:, ['Peso (%)', 'Ticker']]
-pesos_in = pesos_in.set_index('Ticker')
-pesos_in['Peso (%)'] = pesos_in['Peso (%)'] / 100
+for i in archivos:
+    # leer archivos ignorando los dos primeros renglones
+    data = pd.read_csv('files/NAFTRAC_holdings/' + i + '.csv', skiprows=2, header=None)
+    # renombrar las columnas
+    data.columns = list(data.iloc[0, :])
+    # Quitar columnas que no sean nan
+    data = data.loc[:, pd.notnull(data.columns)]
+    data = data.iloc[1:-1].reset_index(drop=True, inplace=False)
+    # Quitar las comas en la columna de precios
+    data['Precio'] = [i.replace(',', '') for i in data['Precio']]
+    # Quitar el * de los Ticker
+    data['Ticker'] = [i.replace('*', '') for i in data['Ticker']]
+    # hacer conversiones de tipos de columnas
+    convert_dict = {'Ticker': str, 'Nombre': str, 'Peso (%)': float, 'Precio': float}
+    data = data.astype(convert_dict)
+    # Convertir a decimal la columna de Peso
+    data['Peso (%)'] = data['Peso (%)']/100
+    # guardar en diccionario
+    data_archivos[i] = data
 
-# All prices (2018-2020)
-i = 0
-for filename in all_files:
-    df = pd.read_csv(filename, skiprows=2)
-    df = df.set_index('Ticker')
-    pesos_in[i] = df.loc[:, 'Precio']
-    i = i + 1
-pesos_in = pesos_in.fillna(0)
-pesos_in = pesos_in.replace(',', '', regex=True)
-pesos_in = pesos_in.astype('float64')
+# fechas a partir de los archivos
+t_fechas = [i.strftime('%d-%m-%y') for i in sorted([pd.to_datetime(i[8:]).date() for i in archivos])]
+# fechas para indexadores
+i_fechas = [i.strftime('%d%m%y') for i in sorted([pd.to_datetime(i[8:]).date() for i in archivos])]
+
+
+tickers = []
+for i in archivos:
+    # i = archivos[0]
+    l_tickers = list(data_archivos[i]['Ticker'])
+    [tickers.append(i + '.MX') for i in l_tickers]
+global_tickers = np.unique(tickers).tolist()
+
+global_tickers = [i.replace('GFREGIOO.MX', 'RA.MX') for i in global_tickers]
+global_tickers = [i.replace('MEXCHEM.MX', 'ORBIA.MX') for i in global_tickers]
+global_tickers = [i.replace('LIVEPOLC.1.MX', 'LIVEPOLC-1.MX') for i in global_tickers]
+
+# eliminar MXN, USD
+[global_tickers.remove(i) for i in ['MXN.MX', 'USD.MX', 'KOFL.MX']]
+# KOFL pasar a Cash
+
+# Bajar precios de Yahoo finance
+inicio = time.time()
+data_yf = yf.download(global_tickers, start='2017-08-21', end='2020-08-22', actions=False,
+                   group_by='close', interval='1d', auto_adjust=True, prepost=False, threads=True)
+print('se tardo', time.time() - inicio, 'segundos')
 
 # Initial commissions
-capital = 1000000
-comision = 0.0125 / 100
-prices = pesos_in
-final = []
+k = 1000000
+c = 0.00125
 
-# Daily prices per weight
-price_per_day = pd.DataFrame()
-price_per_day['Precio'] = prices.loc[:, 0].values  # Comision incial
-price_per_day['Invertido'] = prices['Peso (%)'].values * capital
-price_per_day['Invertido'] = price_per_day['Invertido']
-price_per_day['titulos'] = round(price_per_day['Invertido'] / price_per_day['Precio'])
-price_per_day['Comisiones'] = comision * price_per_day['titulos'] * price_per_day['Precio']
-price_per_day['Total'] = price_per_day['titulos'] * price_per_day['Precio'] - price_per_day['Comisiones']
-price_per_day = price_per_day.fillna(0)
-price_per_day = price_per_day.replace([np.inf, -np.inf], 0)
-final.append(sum(price_per_day['Total']))
+# Solo close data
+data_close = pd.DataFrame()
+for i in global_tickers:
+    data_close[i] = data_yf[i]['Close']
+# Cambiar el index al mismo formato
+data_close.index = data_close.index.strftime('%d%m%y')
+# Quedarnos con solo las fechas de mes (mismas que los archivos)
+month_data_close = data_close.loc[i_fechas, :]
 
-for i in range(1, 32):
-    price_per_day = pd.DataFrame()
-    price_per_day['Precio'] = prices.loc[:, i].values
-    price_per_day['Invertido'] = prices['Peso (%)'].values * capital
-    price_per_day['Invertido'] = price_per_day['Invertido']
-    price_per_day['titulos'] = round(price_per_day['Invertido'] / price_per_day['Precio'])
-    price_per_day['Comisiones'] = 0  # no hay comisiones posteriores
-    price_per_day['Total'] = price_per_day['titulos'] * price_per_day['Precio'] - price_per_day['Comisiones']
-    price_per_day = price_per_day.fillna(0)
-    price_per_day = price_per_day.replace([np.inf, -np.inf], 0)
-    final.append(sum(price_per_day['Total']))
+# Precios a inversion pasiva
+df_precios = pd.DataFrame(data_archivos[archivos[0]][['Ticker', 'Peso (%)']])
+df_precios['Ticker'] = df_precios['Ticker'] + '.MX'
+df_precios = df_precios.replace('GFREGIOO.MX', 'RA.MX')
+df_precios = df_precios.replace('MEXCHEM.MX', 'ORBIA.MX')
+df_precios = df_precios.replace('LIVEPOLC.1.MX', 'LIVEPOLC-1.MX')
+df_precios = df_precios.replace('KOFL.MX', 'CASH')
+df_precios = df_precios.replace('BSMXB.MX', 'CASH')
+df_precios = df_precios.replace('MXN.MX', 'CASH')
+df_precios = df_precios.groupby(df_precios['Ticker']).aggregate({'Ticker': 'first', 'Peso (%)': 'sum'})
+df_precios = df_precios.set_index('Ticker')
 
-final = pd.DataFrame(final)
-final = final / capital - 1
+df_precios['Inversion'] = df_precios['Peso (%)'] * k  # Capital repartido entre pesos
 
-# DF_pasiva
-df_pasiva = dates
-df_pasiva['Capital'] = capital
-df_pasiva['rendcum'] = np.round(final.values*100, 2)
+# Llenar los precios correspondientes
+for i in i_fechas:
+    df_precios[i] = month_data_close.loc[i]
+    df_precios[i] = df_precios[i].fillna(100)  # Rellenar nan's con 100.00 (CASH)
+    df_precios[i] = df_precios['Inversion']/df_precios[i]  # Determinar cantidad de titulos por acción
+    df_precios[i] = df_precios[i].apply(np.floor)  # Redondear hacía abajo la cantidad de titulos
+    df_precios[i] = month_data_close.loc[i] * df_precios[i]  # Titulos * precio
+    df_precios.loc['CASH', i] = df_precios.loc['CASH', 'Inversion']  # Arreglo de nan (CASH)
+    if i == i_fechas[0]:
+        df_precios[i] = df_precios[i] - df_precios[i]*c
+
+sum_df_precios = []
+for i in i_fechas:
+    sum_df_precios.append(sum(df_precios[i]))
+
+sum_df_precios = pd.DataFrame(sum_df_precios)
+sum_df_precios = (sum_df_precios / k-1)*100
+
+df_pasiva = pd.DataFrame()
+df_pasiva['timestamp'] = t_fechas
+df_pasiva['capital'] = k
+df_pasiva['rend_acum'] = sum_df_precios
--- functions.py
+++ functions.py
@@ -12,12 +12,6 @@
 import datetime
 import glob
 
-
-def last_day_of_month(any_day):
-    next_month = any_day.replace(day=28) + datetime.timedelta(days=4)  # this will never fail
-    return next_month - datetime.timedelta(days=next_month.day)
-
-
-def datareader(path):  # DF de precio inicial y ponderaciones
+def datareader(path):  # Import all csv's
     all_files = sorted(glob.glob(path + "/*.csv"))  # path = 'files/NAFTRAC_holdings'
     return all_files
